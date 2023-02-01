6dd21c1baf6284241ef27110e955083ed93da34e
mariamartinez44 <103968661+mariamartinez44@users.noreply.github.com>
2023-02-01 10:08:53 -0500

    Add files via upload

---
+++ taller1/Taller0_Olimpicos/src/uniandes/dpoo/taller0/modificion/Modificacion.java
@@ -0,0 +1,24 @@
+package uniandes.dpoo.taller0.modificion;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import uniandes.dpoo.taller0.procesamiento.CalculadoraEstadisticas;
+import uniandes.dpoo.taller0.procesamiento.LoaderOlimpicos;
+
+public class Modificacion {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CalculadoraEstadisticas calc = null;
+		try {
+			calc = LoaderOlimpicos.cargarArchivo("./data/atletas.csv");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(calc.paisConMasMedallistas());
+	}
+}
